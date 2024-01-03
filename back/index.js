1cb24838af563883f641bb828a21f8b9d3aaaad1
NurekeMuss <146543627+NurekeMuss@users.noreply.github.com>
2024-01-03 23:40:19 +0600

    Update index.js Create login and checking data

--- back/index.js
+++ back/index.js
@@ -1,11 +1,10 @@
 /* using insomnia for CRUD*/
-import express from 'express';
+import express, { request } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt'
 import mongoose from 'mongoose';
 import {registerValidaation} from './validation/auth.js'
 import { validationResult } from "express-validator";
-
 import UserModel from './modules/user.js';
 
 /* connection to monogodb */
@@ -22,9 +21,49 @@ const app = express();
 app.use(express.json());//read json req 
 
 
-/* app.get('/', (req, res) => {
-  res.send("hello world")
-}) */
+/* Create login */
+
+app.post('/auth/login', async (req, res) => {
+  try{
+    const user = await UserModel.findOne({email:req.body.email}); 
+
+    if(!user){
+      return res.status(404).json({
+        message: 'User not found',
+      })
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if(!isValidPass){
+      return res.status(400).json({
+        message: 'Not correct password ot login ',
+      })
+    }
+
+    const token = jwt.sign({
+      _id:user._id,
+    }, 
+    'secret123',//ключ 
+    {
+    expiresIn:'30d', //deadline :)
+    })
+
+    const {passwordHash, ...userData} = user._doc
+
+    res.json({
+      ...userData,
+      token})
+    
+
+  }catch (err){
+    console.log(err);
+    res.status(500).json({
+      message:"cannot sorry registration",
+    })
+  }
+})
+
 
 /* get Login and password through json  */
 app.post('/auth/register', registerValidaation, async (req, res) => {
@@ -92,4 +131,9 @@ app.listen(4444,(err) =>{
 /* ==================== */
 //req - будет храниться о том что мне прислал клиент 
 //res - что я буду передовать клиенту
+
+/* app.get('/', (req, res) => {
+  res.send("hello world")
+}) */
+
 /* ====================== */
